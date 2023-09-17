c62461bf8bd772536bfece22ba80f2f6668e5666
waveydavey_ <davidmammarella12@gmail.com>
2023-09-17 18:51:47 -0400

    Stage_03: data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.mlops_proj1 import logger
 from src.mlops_proj1.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlops_proj1.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlops_proj1.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME_01 = "Data Ingestion stage"
@@ -23,3 +24,12 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME_03 = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME_03} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME_03} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/mlops_proj1/config/configuration.py
+++ src/mlops_proj1/config/configuration.py
@@ -2,6 +2,7 @@ from src.mlops_proj1.constants import *
 from src.mlops_proj1.utils.common import read_yaml, create_directories
 from src.mlops_proj1.entity.config_entity import DataIngestionConfig
 from src.mlops_proj1.entity.config_entity import DataValidationConfig
+from src.mlops_proj1.entity.config_entity import DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -46,3 +47,15 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
