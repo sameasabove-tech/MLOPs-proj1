a6a92bdb58c49c462a3c9b758179331948ad7f87
waveydavey_ <davidmammarella12@gmail.com>
2023-09-18 20:00:40 -0400

    model evaluation with ML Flow finished

--- src/mlops_proj1/components/model_evaluation.py
+++ src/mlops_proj1/components/model_evaluation.py
@@ -55,14 +55,15 @@ class ModelEvaluation:
 
 
             # Model registry does not work with file store
-            if tracking_url_type_store != "file":
+            # if tracking_url_type_store != "file":
 
                 # Register the model
                 # There are other ways to use the Model Registry, which depends on the use case,
                 # please refer to the doc for more information:
                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
-            else:
-                mlflow.sklearn.log_model(model, "model")
+                # mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+            # else:
+            mlflow.sklearn.log_model(model, "model")
 
+            # ^ not sure why above isnt working to regisiter but that worked
     
